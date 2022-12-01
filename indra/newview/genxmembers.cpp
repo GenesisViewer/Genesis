76cde779219673dfeba1350e505607e4d7c404ef
Torric <107435635+TorricRodas@users.noreply.github.com>
2022-12-01 19:37:55 +0000

    Update genxmembers.cpp

--- indra/newview/genxmembers.cpp
+++ indra/newview/genxmembers.cpp
@@ -7,6 +7,8 @@ GenxMembers::GenxMembers()
 	members.push_back("d66dab61-0b8e-427f-82d0-eb5872a73531"); //Billy Arentire
     	members.push_back("0de266c4-8bbe-4ba5-9217-20d22d04f36e"); //Torric Rodas
 	members.push_back("87ae9358-8df0-48e9-a77f-194fe79c6dc1"); //Mely
+	members.push_back("ecf77488-7ead-4e75-80ff-88661bce6037"); //Shep62
+	members.push_back("b0fd5e6d-fb23-4608-9aee-4928225722d3"); //illianna1
 }
 bool GenxMembers::isMember(LLUUID uuid) {
 	auto it = std::find(members.begin(), members.end(), uuid.asString());

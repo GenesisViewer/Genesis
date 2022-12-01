be6d155f47f7181daa3cee39e14a697fdfbf173d
MelyCosti <105198589+MelyCosti@users.noreply.github.com>
2022-12-01 20:11:39 +0100

    Update genxmembers.cpp

--- indra/newview/genxmembers.cpp
+++ indra/newview/genxmembers.cpp
@@ -6,7 +6,7 @@ GenxMembers::GenxMembers()
 {
 	members.push_back("d66dab61-0b8e-427f-82d0-eb5872a73531"); //Billy Arentire
     	members.push_back("0de266c4-8bbe-4ba5-9217-20d22d04f36e"); //Torric Rodas
-	members.push_back("87ae9358-8df0-48e9-a77f-194fe79c6dc1"); //Torric Rodas
+	members.push_back("87ae9358-8df0-48e9-a77f-194fe79c6dc1"); //Mely
 }
 bool GenxMembers::isMember(LLUUID uuid) {
 	auto it = std::find(members.begin(), members.end(), uuid.asString());

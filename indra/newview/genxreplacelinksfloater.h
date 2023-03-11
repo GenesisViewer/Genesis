b2be01d0f6daa9cd3a5478db15bdf2bd0c51e602
Melanie Costi <melycostisl@gmail.com>
2023-03-11 16:51:26 +0100

    implementing replace links, inspiration from Firestorm Viewer

--- indra/newview/genxreplacelinksfloater.h
+++ indra/newview/genxreplacelinksfloater.h
@@ -13,9 +13,15 @@ public:
     void onInventoryItemDropped();
     void onCloseButton();
     void onReplaceButton();
+    void decreaseCounter(){mCounter--;}
+    int getCounter() {return mCounter;}
+    static void itemRemovedCallback(LLHandle<GenxFloaterReplaceLinks> floater_handle,const LLUUID& outfit_folder_id,const LLUUID& baseoutfit_id);
+    static void createLinkCallback(LLHandle<GenxFloaterReplaceLinks> floater_handle,const LLUUID& link_id,const LLUUID& outfit_folder_id,const LLUUID& baseoutfit_id);
 private:
     std::string link_id;    
     LLUUID mLinkedItemsToReplaceOriginalUUID;
     LLUUID mReplaceByOriginalUUID;
+    void countItemLinks();
+    int mCounter;
 };
 #endif

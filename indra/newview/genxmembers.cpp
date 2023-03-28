7090c3189e750c2fb532e151d525c8e38b6908e1
Torric <107435635+TorricRodas@users.noreply.github.com>
2023-03-28 20:46:43 +0100

    Update genxmembers.cpp

--- indra/newview/genxmembers.cpp
+++ indra/newview/genxmembers.cpp
@@ -10,7 +10,6 @@ GenxMembers::GenxMembers()
 	members.push_back("ecf77488-7ead-4e75-80ff-88661bce6037"); //Shep62
 	members.push_back("b0fd5e6d-fb23-4608-9aee-4928225722d3"); //illianna1
 	members.push_back("82d88055-7897-4e5b-8804-e5befe86d92f"); //Coffeequeen68
-	members.push_back("96f9c1a5-bad5-4e71-a491-07382979020c"); //LadyDeeFul
 	members.push_back("3ac2cf73-eefa-4102-8016-7676a9a0081e"); //candicek15
 	members.push_back("a524cfd5-0e03-4c2a-865c-747bb2fa0f16"); //biyayo
 	members.push_back("64bcffb1-6466-40fe-affd-b97319692c55"); //Jazzi Slade

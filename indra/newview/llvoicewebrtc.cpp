82deadcc5444920262d5e544a57e7481ff0fb8d7
MelyCosti <105198589+MelyCosti@users.noreply.github.com>
2024-04-28 11:41:10 +0200

    continue implementing webrtc

--- indra/newview/llvoicewebrtc.cpp
+++ indra/newview/llvoicewebrtc.cpp
@@ -1,6 +1,14 @@
 #include "llviewerprecompiledheaders.h"
 #include "llvoicewebrtc.h"
 bool LLWebRTCVoiceClient::sShuttingDown = false;
+
+// Defines the maximum number of times(in a row) "stateJoiningSession" case for spatial channel is reached in stateMachine()
+// which is treated as normal. If this number is exceeded we suspect there is a problem with connection
+// to voice server (EXT-4313). When voice works correctly, there is from 1 to 15 times. 50 was chosen
+// to make sure we don't make mistake when slight connection problems happen- situation when connection to server is
+// blocked is VERY rare and it's better to sacrifice response time in this situation for the sake of stability.
+const int MAX_NORMAL_JOINING_SPATIAL_NUM = 50;
+
 LLWebRTCVoiceClient::LLWebRTCVoiceClient(){
 
     mVoiceVersion.serverVersion = "";
@@ -58,4 +66,325 @@ void LLWebRTCVoiceClient::setEarLocation(S32 loc)
         mEarLocation        = loc;
         mSpatialCoordsDirty = true;
     }
+}
+void LLWebRTCVoiceClient::sessionTerminate()
+{
+	mSessionTerminateRequested = true;
+}
+void LLWebRTCVoiceClient::tuningStart()
+{
+	mTuningMode = true;
+	LL_DEBUGS("Voice") << "Starting tuning" << LL_ENDL;
+	if(getState() >= stateNoChannel)
+	{
+		LL_DEBUGS("Voice") << "no channel" << LL_ENDL;
+		sessionTerminate();
+	}
+}
+
+void LLWebRTCVoiceClient::tuningStop()
+{
+	mTuningMode = false;
+}
+
+bool LLWebRTCVoiceClient::inTuningMode()
+{
+	bool result = false;
+	switch(getState())
+	{
+	case stateMicTuningRunning:
+		result = true;
+		break;
+	default:
+		break;
+	}
+	return result;
+}
+std::string LLWebRTCVoiceClient::state2string(LLWebRTCVoiceClient::state inState)
+{
+	std::string result = "UNKNOWN";
+
+		// Prevent copy-paste errors when updating this list...
+#define CASE(x)  case x:  result = #x;  break
+
+	switch(inState)
+	{
+		CASE(stateDisableCleanup);
+		CASE(stateDisabled);
+		CASE(stateStart);
+		CASE(stateDaemonLaunched);
+		CASE(stateConnecting);
+		CASE(stateConnected);
+		CASE(stateIdle);
+		CASE(stateMicTuningStart);
+		CASE(stateMicTuningRunning);
+		CASE(stateMicTuningStop);
+		CASE(stateCaptureBufferPaused);
+		CASE(stateCaptureBufferRecStart);
+		CASE(stateCaptureBufferRecording);
+		CASE(stateCaptureBufferPlayStart);
+		CASE(stateCaptureBufferPlaying);
+		CASE(stateConnectorStart);
+		CASE(stateConnectorStarting);
+		CASE(stateConnectorStarted);
+		CASE(stateLoginRetry);
+		CASE(stateLoginRetryWait);
+		CASE(stateNeedsLogin);
+		CASE(stateLoggingIn);
+		CASE(stateLoggedIn);
+		CASE(stateVoiceFontsWait);
+		CASE(stateVoiceFontsReceived);
+		CASE(stateCreatingSessionGroup);
+		CASE(stateNoChannel);
+		CASE(stateRetrievingParcelVoiceInfo);
+		CASE(stateJoiningSession);
+		CASE(stateSessionJoined);
+		CASE(stateRunning);
+		CASE(stateLeavingSession);
+		CASE(stateSessionTerminated);
+		CASE(stateLoggingOut);
+		CASE(stateLoggedOut);
+		CASE(stateConnectorStopping);
+		CASE(stateConnectorStopped);
+		CASE(stateConnectorFailed);
+		CASE(stateConnectorFailedWaiting);
+		CASE(stateLoginFailed);
+		CASE(stateLoginFailedWaiting);
+		CASE(stateJoinSessionFailed);
+		CASE(stateJoinSessionFailedWaiting);
+		CASE(stateJail);
+	}
+
+#undef CASE
+
+	return result;
+}
+void LLWebRTCVoiceClient::setState(state inState)
+{
+	LL_DEBUGS("Voice") << "entering state " << state2string(inState) << LL_ENDL;
+
+	mState = inState;
+}
+bool LLWebRTCVoiceClient::isVoiceWorking() const
+{
+	//Added stateSessionTerminated state to avoid problems with call in parcels with disabled voice (EXT-4758)
+	// Condition with joining spatial num was added to take into account possible problems with connection to voice
+	// server(EXT-4313). See bug descriptions and comments for MAX_NORMAL_JOINING_SPATIAL_NUM for more info.
+	return (mSpatialJoiningNum < MAX_NORMAL_JOINING_SPATIAL_NUM) && (stateLoggedIn <= mState) && (mState <= stateSessionTerminated);
+}
+static int scale_mic_volume(float volume)
+{
+	// incoming volume has the range [0.0 ... 2.0], with 1.0 as the default.
+	// Map it to Vivox levels as follows: 0.0 -> 30, 1.0 -> 50, 2.0 -> 70
+	return 30 + (int)(volume * 20.0f);
+}
+void LLWebRTCVoiceClient::tuningSetMicVolume(float volume)
+{
+	int scaled_volume = scale_mic_volume(volume);
+
+	if(scaled_volume != mTuningMicVolume)
+	{
+		mTuningMicVolume = scaled_volume;
+		mTuningMicVolumeDirty = true;
+	}
+}
+static int scale_speaker_volume(float volume)
+{
+	// incoming volume has the range [0.0 ... 1.0], with 0.5 as the default.
+	// Map it to Vivox levels as follows: 0.0 -> 30, 0.5 -> 50, 1.0 -> 70
+	return 30 + (int)(volume * 40.0f);
+
+}
+void LLWebRTCVoiceClient::tuningSetSpeakerVolume(float volume)
+{
+	int scaled_volume = scale_speaker_volume(volume);
+
+	if(scaled_volume != mTuningSpeakerVolume)
+	{
+		mTuningSpeakerVolume = scaled_volume;
+		mTuningSpeakerVolumeDirty = true;
+	}
+}
+float LLWebRTCVoiceClient::tuningGetEnergy(void)
+{
+	return mTuningEnergy;
+}
+void LLWebRTCVoiceClient::updateDefaultBoostLevel(float volume) {
+	if (LLVoiceClient::getInstance()->getDefaultBoostlevel() == volume)
+		return;
+	if (!mAudioSession) 
+		return;	
+	
+
+	//apply the diff to participant but value each participant must be between 0 and 1
+	participantList::iterator iter = mAudioSession->mParticipantList.begin();
+
+		
+	
+	for(; iter != mAudioSession->mParticipantList.end(); iter++)
+	{
+		participantState *p = &*iter;
+		if (p && p->isAvatar()) {
+			
+			float newVolume=llclamp(volume,0.0f,1.0f);
+			LLVoiceClient::getInstance()->setUserVolume(p->mAvatarID,newVolume);
+		}
+	}
+	
+}
+void LLWebRTCVoiceClient::getParticipantList(uuid_set_t &participants)
+{
+	if(mAudioSession)
+	{
+		// Singu Note: mParticipantList has replaced mParticipantsByURI.
+		for (participantList::iterator iter = mAudioSession->mParticipantList.begin();
+			iter != mAudioSession->mParticipantList.end();
+			iter++)
+		{
+			participants.insert(iter->mAvatarID);
+		}
+	}
+}
+LLWebRTCVoiceClient::participantState* LLWebRTCVoiceClient::sessionState::findParticipantByID(const LLUUID& id)
+{
+	// Singu Note: mParticipantList has replaced mParticipantsByUUID.
+	participantList& vec = mParticipantList;
+	participantList::iterator iter = std::find_if(vec.begin(), vec.end(), boost::bind(&participantList::value_type::mAvatarID, _1) == id);
+
+	if(iter != mParticipantList.end())
+	{
+		return &*iter;
+	}
+
+	return NULL;
+}
+LLWebRTCVoiceClient::participantState* LLWebRTCVoiceClient::findParticipantByID(const LLUUID& id)
+{
+	return mAudioSession ? mAudioSession->findParticipantByID(id) : NULL;
+}
+bool LLWebRTCVoiceClient::isParticipant(const LLUUID &speaker_id)
+{
+	// Singu Note: findParticipantByID does the same thing. If null, returns false.
+	return findParticipantByID(speaker_id);
+}
+bool LLWebRTCVoiceClient::participantState::isAvatar()
+{
+	return mAvatarIDValid;
+}
+bool LLWebRTCVoiceClient::isPreviewPlaying()
+{
+	return (mCaptureBufferMode && mCaptureBufferPlaying);
+}
+bool LLWebRTCVoiceClient::isPreviewRecording()
+{
+	return (mCaptureBufferMode && mCaptureBufferRecording);
+}
+void LLWebRTCVoiceClient::stopPreviewBuffer()
+{
+	mCaptureBufferRecording = false;
+	mCaptureBufferPlaying = false;
+}
+void LLWebRTCVoiceClient::playPreviewBuffer(const LLUUID& effect_id)
+{
+	if (!mCaptureBufferMode)
+	{
+		LL_DEBUGS("Voice") << "Not in voice effect preview mode, no buffer to play." << LL_ENDL;
+		mCaptureBufferRecording = false;
+		return;
+	}
+
+	if (!mCaptureBufferRecorded)
+	{
+		// Can't play until we have something recorded!
+		mCaptureBufferPlaying = false;
+		return;
+	}
+
+	mPreviewVoiceFont = effect_id;
+	mCaptureBufferPlaying = true;
+}
+void LLWebRTCVoiceClient::enablePreviewBuffer(bool enable)
+{
+	mCaptureBufferMode = enable;
+	if(mCaptureBufferMode && getState() >= stateNoChannel)
+	{
+		LL_DEBUGS("Voice") << "no channel" << LL_ENDL;
+		sessionTerminate();
+	}
+}
+void LLWebRTCVoiceClient::recordPreviewBuffer()
+{
+	if (!mCaptureBufferMode)
+	{
+		LL_DEBUGS("Voice") << "Not in voice effect preview mode, cannot start recording." << LL_ENDL;
+		mCaptureBufferRecording = false;
+		return;
+	}
+
+	mCaptureBufferRecording = true;
+}
+void LLWebRTCVoiceClient::addObserver(LLVoiceEffectObserver* observer)
+{
+	mVoiceFontObservers.insert(observer);
+}
+
+void LLWebRTCVoiceClient::removeObserver(LLVoiceEffectObserver* observer)
+{
+	mVoiceFontObservers.erase(observer);
+}
+const voice_effect_list_t& LLWebRTCVoiceClient::getVoiceEffectList() const
+{
+	return mVoiceFontList;
+}
+
+const voice_effect_list_t& LLWebRTCVoiceClient::getVoiceEffectTemplateList() const
+{
+	return mVoiceFontTemplateList;
+}
+void LLWebRTCVoiceClient::deleteAllVoiceFonts()
+{
+	mVoiceFontList.clear();
+
+	voice_font_map_t::iterator iter;
+	for (iter = mVoiceFontMap.begin(); iter != mVoiceFontMap.end(); ++iter)
+	{
+		delete iter->second;
+	}
+	mVoiceFontMap.clear();
+}
+
+void LLWebRTCVoiceClient::deleteVoiceFontTemplates()
+{
+	mVoiceFontTemplateList.clear();
+
+	voice_font_map_t::iterator iter;
+	for (iter = mVoiceFontTemplateMap.begin(); iter != mVoiceFontTemplateMap.end(); ++iter)
+	{
+		delete iter->second;
+	}
+	mVoiceFontTemplateMap.clear();
+}
+void LLWebRTCVoiceClient::refreshVoiceEffectLists(bool clear_lists)
+{
+	if (clear_lists)
+	{
+		mVoiceFontsReceived = false;
+		deleteAllVoiceFonts();
+		deleteVoiceFontTemplates();
+	}
+
+	accountGetSessionFontsSendMessage();
+	accountGetTemplateFontsSendMessage();
+}
+void LLWebRTCVoiceClient::accountGetSessionFontsSendMessage()
+{
+	//TODO
+    //WebRTC doesn't support effects, do nothing
+}
+
+void LLWebRTCVoiceClient::accountGetTemplateFontsSendMessage()
+{
+	//TODO
+    //WebRTC doesn't support effects, do nothing
+	
 }
--- indra/newview/llvoicewebrtc.h
+++ indra/newview/llvoicewebrtc.h
@@ -218,15 +218,232 @@ public:
 
 	//@}
 	void setEarLocation(S32 loc);
+
+		// internal state for a simple state machine.  This is used to deal with the asynchronous nature of some of the messages.
+	// Note: if you change this list, please make corresponding changes to LLVivoxVoiceClient::state2string().
+	enum state
+	{
+		stateDisableCleanup,
+		stateDisabled,				// Voice is turned off.
+		stateStart,					// Class is initialized, socket is created
+		stateDaemonLaunched,		// Daemon has been launched
+		stateConnecting,			// connect() call has been issued
+		stateConnected,				// connection to the daemon has been made, send some initial setup commands.
+		stateIdle,					// socket is connected, ready for messaging
+		stateMicTuningStart,
+		stateMicTuningRunning,
+		stateMicTuningStop,
+		stateCaptureBufferPaused,
+		stateCaptureBufferRecStart,
+		stateCaptureBufferRecording,
+		stateCaptureBufferPlayStart,
+		stateCaptureBufferPlaying,
+		stateConnectorStart,		// connector needs to be started
+		stateConnectorStarting,		// waiting for connector handle
+		stateConnectorStarted,		// connector handle received
+		stateLoginRetry,			// need to retry login (failed due to changing password)
+		stateLoginRetryWait,		// waiting for retry timer
+		stateNeedsLogin,			// send login request
+		stateLoggingIn,				// waiting for account handle
+		stateLoggedIn,				// account handle received
+		stateVoiceFontsWait,		// Awaiting the list of voice fonts
+		stateVoiceFontsReceived,	// List of voice fonts received
+		stateCreatingSessionGroup,	// Creating the main session group
+		stateNoChannel,				// Need to join a channel
+		stateRetrievingParcelVoiceInfo,    // waiting for parcel voice info request to return with spatial credentials
+		stateJoiningSession,		// waiting for session handle
+		stateSessionJoined,			// session handle received
+		stateRunning,				// in session, steady state
+		stateLeavingSession,		// waiting for terminate session response
+		stateSessionTerminated,		// waiting for terminate session response
+
+		stateLoggingOut,			// waiting for logout response
+		stateLoggedOut,				// logout response received
+		stateConnectorStopping,		// waiting for connector stop
+		stateConnectorStopped,		// connector stop received
+
+		// We go to this state if the login fails because the account needs to be provisioned.
+
+		// error states.  No way to recover from these yet.
+		stateConnectorFailed,
+		stateConnectorFailedWaiting,
+		stateLoginFailed,
+		stateLoginFailedWaiting,
+		stateJoinSessionFailed,
+		stateJoinSessionFailedWaiting,
+
+		stateJail					// Go here when all else has failed.  Nothing will be retried, we're done.
+	};
+	struct participantState
+	{
+	public:
+		// Singu Note: Extended the ctor.
+		participantState(const std::string &uri, const LLUUID& id, bool isAv);
+
+		bool updateMuteState();	// true if mute state has changed
+		bool isAvatar();
+
+		std::string mURI;
+		LLUUID mAvatarID;
+		std::string mAccountName;
+		std::string mDisplayName;
+		LLFrameTimer mSpeakingTimeout;
+		F32	mLastSpokeTimestamp;
+		F32 mPower;
+		F32 mVolume;
+		std::string mGroupID;
+		int mUserVolume;
+		
+		bool mPTT;
+		bool mIsSpeaking;
+		bool mIsModeratorMuted;
+		bool mOnMuteList;		// true if this avatar is on the user's mute list (and should be muted)
+		bool mVolumeSet;		// true if incoming volume messages should not change the volume
+		bool mVolumeDirty;		// true if this participant needs a volume command sent (either mOnMuteList or mUserVolume has changed)
+		bool mAvatarIDValid;
+		bool mIsSelf;
+		bool isBuddy;
+	};
+	// Singu Note: participantList has replaced both participantMap and participantUUIDMap.
+	typedef std::vector<participantState> participantList;
+
+	struct sessionState
+	{
+	public:
+		sessionState();
+		~sessionState();
+
+		participantState *addParticipant(const std::string &uri);
+		// Note: after removeParticipant returns, the participant* that was passed to it will have been deleted.
+		// Take care not to use the pointer again after that.
+		// Singu Note: removeParticipant now internally finds the entry. More efficient. Other option is to add a find procedure that returns an iterator.
+		void removeParticipant(const std::string& uri);
+		void removeAllParticipants();
+
+		participantState *findParticipant(const std::string &uri);
+		participantState *findParticipantByID(const LLUUID& id);
+
+		bool isCallBackPossible();
+		bool isTextIMPossible();
+
+		std::string mHandle;
+		std::string mGroupHandle;
+		std::string mSIPURI;
+		std::string mAlias;
+		std::string mName;
+		std::string mAlternateSIPURI;
+		std::string mHash;			// Channel password
+		std::string mErrorStatusString;
+		std::queue<std::string> mTextMsgQueue;
+
+		LLUUID		mIMSessionID;
+		LLUUID		mCallerID;
+		int			mErrorStatusCode;
+		int			mMediaStreamState;
+		int			mTextStreamState;
+		bool		mCreateInProgress;	// True if a Session.Create has been sent for this session and no response has been received yet.
+		bool		mMediaConnectInProgress;	// True if a Session.MediaConnect has been sent for this session and no response has been received yet.
+		bool		mVoiceInvitePending;	// True if a voice invite is pending for this session (usually waiting on a name lookup)
+		bool		mTextInvitePending;		// True if a text invite is pending for this session (usually waiting on a name lookup)
+		bool		mSynthesizedCallerID;	// True if the caller ID is a hash of the SIP URI -- this means we shouldn't do a name lookup.
+		bool		mIsChannel;	// True for both group and spatial channels (false for p2p, PSTN)
+		bool		mIsSpatial;	// True for spatial channels
+		bool		mIsP2P;
+		bool		mIncoming;
+		bool		mVoiceEnabled;
+		bool		mReconnect;	// Whether we should try to reconnect to this session if it's dropped
+
+		// Set to true when the volume/mute state of someone in the participant list changes.
+		// The code will have to walk the list to find the changed participant(s).
+		bool		mVolumeDirty;
+		bool		mMuteDirty;
+		bool		mNonFriendsAttenuationLevelDirty;
+
+		bool		mParticipantsChanged;
+		// Singu Note: mParticipantList has replaced both mParticipantsByURI and mParticipantsByUUID.
+		participantList mParticipantList;
+
+		LLUUID		mVoiceFontID;
+	};
 private:
 	llwebrtc::LLWebRTCDeviceInterface *mWebRTCDeviceInterface;
 	LLVoiceVersionInfo mVoiceVersion;
-	bool  mVoiceEnabled;
 
+	sessionState *mAudioSession;		// Session state for the current audio session
+	bool  mVoiceEnabled;
+	bool mTuningMode;
     S32   mEarLocation;	
 	bool mSpatialCoordsDirty;
 	// These variables can last longer than WebRTC in coroutines so we need them as static
     static bool sShuttingDown;	
+
+
+	// Number of times (in a row) "stateJoiningSession" case for spatial channel is reached in stateMachine().
+	// The larger it is the greater is possibility there is a problem with connection to voice server.
+	// Introduced while fixing EXT-4313.
+	int mSpatialJoiningNum;
+	bool mSessionTerminateRequested;
+	bool mRelogRequested;
+	state mState;
+
+	bool mCaptureBufferMode;		// Disconnected from voice channels while using the capture buffer.
+	bool mCaptureBufferRecording;	// A voice sample is being captured.
+	bool mCaptureBufferRecorded;	// A voice sample is captured in the buffer ready to play.
+	bool mCaptureBufferPlaying;		// A voice sample is being played.
+
+	LLUUID mPreviewVoiceFont;
+	LLUUID mPreviewVoiceFontLast;
+
+	int mTuningMicVolume;
+	bool mTuningMicVolumeDirty;
+	float mTuningEnergy;
+	int mTuningSpeakerVolume;
+	bool mTuningSpeakerVolumeDirty;
+	void setState(state inState);
+	state getState(void)  { return mState; };
+	std::string state2string(state inState);
+
+	// Pokes the state machine to leave the audio session next time around.
+	void sessionTerminate();
+
+	participantState *findParticipantByID(const LLUUID& id);
+
+
+
+
+	typedef std::set<LLVoiceEffectObserver*> voice_font_observer_set_t;
+	voice_font_observer_set_t mVoiceFontObservers;
+
+	void accountGetSessionFontsSendMessage();
+	void accountGetTemplateFontsSendMessage();
+	bool mVoiceFontsReceived;
+	bool mVoiceFontsNew;
+	bool mVoiceFontListDirty;
+	voice_effect_list_t	mVoiceFontList;
+	voice_effect_list_t	mVoiceFontTemplateList;
+
+	struct voiceFontEntry
+	{
+		voiceFontEntry(LLUUID& id);
+		~voiceFontEntry();
+
+		LLUUID		mID;
+		S32			mFontIndex;
+		std::string mName;
+		LLDate		mExpirationDate;
+		S32			mFontType;
+		S32			mFontStatus;
+		bool		mIsNew;
+
+		LLFrameTimer	mExpiryTimer;
+		LLFrameTimer	mExpiryWarningTimer;
+	};
+	typedef std::map<const LLUUID, voiceFontEntry*> voice_font_map_t;
+	voice_font_map_t	mVoiceFontMap;
+	voice_font_map_t	mVoiceFontTemplateMap;
+	
+	void deleteAllVoiceFonts();
+	void deleteVoiceFontTemplates();
 };
 
 #endif //LL_VOICE_WEBRTC_H
